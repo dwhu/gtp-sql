722434f355e4d245960ec9688b8b443c1d9f5088
Dave Hughes <hughes.david0@gmail.com>
2023-08-17 13:42:29 -0700

    Updating to issue sql against db

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from typing import Union
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.llms.openai import OpenAI
+from langchain.agents.agent_types import AgentType
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+llm = OpenAI(temperature=0)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+db = SQLDatabase.from_uri("postgresql://app:@localhost/db")
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+agent_executor = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/query")
+def read_root(query: str):
+    out = agent_executor.run(query)
+    return {"response": out}
